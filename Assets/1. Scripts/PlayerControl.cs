ce4c5533a040305bb0f47013a9a354890b909e04
HJ <104305435+w8err@users.noreply.github.com>
2023-11-12 01:31:42 +0900

    Sample Scene 플레이어 추가 PlayerControl 스크립트 추가

---
+++ Assets/1. Scripts/PlayerControl.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("플레이어 컨트롤 코드입니다.");
+        
+    }
+
+}
